d3f9b3735e66f7756d7001e642f524b0a63b3e28
Tarunkumar B T <basapathitarunkumar9686@gmail.com>
2024-03-31 23:07:54 +0530

    requirements.txt

--- fooddeli/app/views.py
+++ fooddeli/app/views.py
@@ -1,21 +1,28 @@
 from django.http import JsonResponse
 from .models import Pricing
+from rest_framework.decorators import api_view
 
+@api_view(['POST'])
 def calculate_price(request):
-    total_distance = 12
+
+    zone = request.data.get('zone')
+    organization_id = request.data.get('organization_id')
+    total_distance = float(request.data.get('total_distance'))  # Convert to float
+    item_type = request.data.get('item_type')
+
     try:
-        pricing = Pricing.objects.get(pk=1)
+        pricing = Pricing.objects.get(pk=organization_id)
     except Pricing.DoesNotExist:
         return JsonResponse({'error': 'Pricing details not found'}, status=400)
 
     total_price = pricing.base_price
     if total_distance > pricing.base_distance_in_km:
         additional_distance = total_distance - pricing.base_distance_in_km
-        if pricing.item.type == 'perishable':  # Access 'item' attribute of pricing object
+        if pricing.item.type == 'perishable':
             per_km_price = pricing.km_price_perishable
         else:
-            per_km_price = pricing.km_price_non_perishable  # Corrected the assignment here
-        total_price += additional_distance * per_km_price
+            per_km_price = pricing.km_price_non_perishable
+        total_price=float(additional_distance)*float(per_km_price)
+        total_price+=float(pricing.base_price)
 
-    print(total_price)
     return JsonResponse({'total_price': total_price})
